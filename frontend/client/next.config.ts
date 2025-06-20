6b3b981a0ee04ca57707349771fdb045f35efdcd
deba7 <debayudhbasu@gmail.com>
2025-06-20 23:08:09 +0530

    changes to auth

--- frontend/client/next.config.ts
+++ frontend/client/next.config.ts
@@ -1,7 +1,12 @@
+import { createCivicAuthPlugin } from "@civic/auth/nextjs"
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withCivicAuth = createCivicAuthPlugin({
+  clientId: "591fba43-f526-4a18-b303-e0ad4dd98bd9"
+});
+
+export default withCivicAuth(nextConfig)
